8f2a62bafe6d0a5970ec3f901fe9f115d80972fb
nadya <nadya.vilboa@list.ru>
2021-08-01 20:47:35 +0300

    Доделывает стили текста

---
+++ source/js/main.js
@@ -0,0 +1,13 @@
+import { toggleMenu } from './mobile-menu.js';
+import { initSlider } from './slider.js';
+
+const buttonToggle = document.querySelector('.page-header__toggle');
+
+const buttonEarly = document.querySelector('.slider__button--early');
+const buttonToday = document.querySelector('.slider__button--today');
+
+buttonToggle.addEventListener('click', toggleMenu);
+
+if(buttonEarly && buttonToday) {
+  initSlider();
+}
--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,10 +1,11 @@
 const menu = document.querySelector('.main-nav');
-const buttonToggler = document.querySelector('.page-header__toggler');
 
-buttonToggler.addEventListener('click', () => {
-  if(menu.classList.contains('visually-hidden')) {
-    menu.classList.remove('visually-hidden');
+const toggleMenu = () => {
+  if(menu.classList.contains('display-none')) {
+    menu.classList.remove('display-none');
   } else {
-    menu.classList.add('visually-hidden');
+    menu.classList.add('display-none');
   }
-});
+}
+
+export { toggleMenu };
